3a60079af5a3b38ad60112bb7bef4ac3c48ba301
Psolisc <92132934+Psolisc@users.noreply.github.com>
2022-10-23 23:49:06 -0600

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "ChallengeVPC"
