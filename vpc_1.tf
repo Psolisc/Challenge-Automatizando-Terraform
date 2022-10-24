91a08663ec084f06ce5e71b03f194bdbfed1d548
Psolisc <92132934+Psolisc@users.noreply.github.com>
2022-10-23 20:19:36 -0600

    Update vpc_1.tf

--- vpc_1.tf
+++ vpc_1.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main-vpc" {
     cidr_block       = "172.16.0.0/16"
     instance_tenancy = "default"
 
     tags = {
-        name = "main"
+        name = "ChallengeVPC"
     }
 }
